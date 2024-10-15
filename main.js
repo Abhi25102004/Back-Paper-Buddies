a0448ed54e46e9c03abc5567013de7f7bde45ffb
Abhishek <7005093380a@gmail.com>
2024-10-15 19:28:51 +0530

    Adding bootstrap

--- main.js
+++ main.js
@@ -1,51 +1,33 @@
 // course list of objects 
 let lessons =[
     {
-        "id":1,
         "name":"1ST SEMESTER MATHEMATICS",
         "details":"Algebra-I and Calculus-I",
-        "semester":"1",
-        "subject":"Mathematics",
         "contents":"https://youtube.com/playlist?list=PL8tjymE0TCzBJbjVyOiDrHPFX76rO3TF_&si=pKoxt2YhRLj1kfbZ",
         "syllabus":"Maths_1.pdf"
     },
     {
-        "id":2,
         "name":"1ST SEMESTER PHYSICS",
         "details":"Mathematical Physics-I, Mechanics, Waves and Acoustics",
-        "semester":"1",
-        "subject":"Physics",
         "contents":"https://youtube.com/playlist?list=PL8tjymE0TCzAE0qPVXMFUNznv0zESb2Fq&si=myKQU44WsO46xdIu",
         "syllabus":"Physics_Syllabus_1.pdf"
     },
     {
-        "id":3,
         "name":"3RD SEMESTER MATHEMATICS",
         "details":"Statics and Calculus-II",
-        "semester":"3",
-        "subject":"Mathematics",
         "contents":"https://youtube.com/playlist?list=PL8tjymE0TCzDv2V5DrOvfyMvVZQtKwY2K&si=wfUe5jmfCetp3rUv",
         "syllabus":"Maths_3.pdf"
     },
     {
-        "id":4,
         "name":"3RD SEMESTER PHYSICS",
         "details":"Thermal Physics, Optics",
-        "semester":"3",
-        "subject":"Physics",
         "contents":"https://youtube.com/playlist?list=PL8tjymE0TCzCIVjZ8qz5BZGW1B9kqDcBY&si=XHiGE2rjBPizYbR5",
         "syllabus":"Physics_Syllabus_3.pdf"
     }
 ]
 
-
-if(!localStorage.getItem('PopularCourses')){
-    localStorage.setItem('PopularCourses',[1,2,3]);
-}
-
 function gotoCoursePage(id){
-    let indexId = id - 1;
-    let values = lessons[indexId];
+    let values = lessons[id];
     let list = JSON.parse("["+localStorage.getItem('PopularCourses')+"]");
     if(!list.includes(id)){
         list.shift();
@@ -58,101 +40,32 @@ function gotoCoursePage(id){
 const fullPath = window.location.pathname;
 const fileName = fullPath.substring(fullPath.lastIndexOf('/') + 1);
 
-if(fileName == "index.html" || fileName == ""){
-    const doc = document.querySelector(".courses")
-    lessons.forEach((lesson) => {
-        if(localStorage.getItem('PopularCourses').includes(lesson.id)){
-            doc.innerHTML +=`<div class="course-card">
-            <h3>${lesson.name}</h3> 
-            <p>${lesson.details}</p> 
-            <button class="btn-link" onclick = "gotoCoursePage(${lesson.id})" >Enroll Now</button> 
-            </div>`;
-        }
-    })    
-}
-else if(fileName == "courses.html"){
-
-    let valueSemester = "";
-    let valueSubject = "";
-
-    function selectedOptions(semester , subject){
-        let list = []; 
-        if (semester == "" && subject == ""){
-            list = [1,2,3,4];
-        }
-        else if(semester == "" && subject != ""){
-            lessons.forEach((lesson) => {
-                if(lesson.subject == subject){
-                   list.push(lesson.id); 
-                }
-            });
-        }
-        else if(semester != "" && subject == ""){
-            lessons.forEach((lesson) => {
-                if(lesson.semester == semester){
-                    list.push(lesson.id);
-                }
-            });
-        }
-        else{
-            lessons.forEach((lesson) => {
-                if(lesson.subject == subject && lesson.semester == semester){
-                    list.push(lesson.id);
-                }
-            });
-        }
-        console.log(list);
-        return list;
-    }
-
-    function AddingLesson(LessonList){
-        const doc = document.querySelector(".course-list");
-        doc.innerHTML = "";
-        lessons.forEach((lesson) => {
-            if(LessonList.includes(lesson.id)){
-                doc.innerHTML +=`<div class="course-card">
-                <h3>${lesson.name}</h3> 
-                <p>${lesson.details}</p> 
-                <button class="btn-link" onclick = "gotoCoursePage(${lesson.id})" >Enroll Now</button> 
-                </div>`;
-            }
-        })
-    }
-
-    document.querySelector("#semester").onchange = function(){
-        valueSemester = this.value;
-        AddingLesson(selectedOptions(valueSemester , valueSubject));
-    }
-
-    document.querySelector("#subject").onchange = function(){
-        valueSubject = this.value;
-        AddingLesson(selectedOptions(valueSemester , valueSubject));
-    }
-
-    AddingLesson([1,2,3,4]);
-
-    console.log(typeof(localStorage.getItem('PopularCourses')));
-}
-else{
+if(fileName == "courseDetailPage.html"){
     const urlParams = new URLSearchParams(window.location.search);
     const name = urlParams.get('name');
     const details = urlParams.get('details');
     const playlist = urlParams.get('playlist');
     const syllabus = urlParams.get('syllabus');
     
-    document.querySelector('.course-detail').innerHTML=`<div class="course-info">
-    <div>
-        <h1 class="course-title">${name}</h1>
-        <p>${details}</p>
-    </div>
-    </div>
-
-    <div class="resources">
-    <h3>Additional Resources</h3>
-    <ul>
-        <li><a href="${syllabus}">Download Course Syllabus</a></li>
-        <li><a href="${playlist}">Video Playlist</a></li>
-        <li><a href="https://anthonys.ac.in/pages/facilities/fclt_library.php">Previous Year Question Papers</a></li>
-    </ul>
-    </div>`
-}    
+    document.querySelector('#AddInformation').innerHTML=`<div class="row mb-4">
+            <div class="col-12 text-center">
+                <h1 class="course-title">${name}</h1>
+                <p class="lead">${details}</p>
+            </div>
+        </div>
+        
+        <div class="resources">
+            <h3 class="text-center">Additional Resources</h3>
+            <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">
+                    <a href="${syllabus}" class="text-decoration-none">Download Course Syllabus</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="${playlist}" class="text-decoration-none">Video Playlist</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="https://anthonys.ac.in/pages/facilities/fclt_library.php" class="text-decoration-none">Previous Year Question Papers</a>
+                </li>
+            </ul>
+        </div>`;
+}
